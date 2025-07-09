1855fe4b985a354c7df13ef2cf6d31ee14f899e4
DurgaPavan14 <95292418+DurgaPavan14@users.noreply.github.com>
2025-07-09 19:47:10 -0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,8 @@
+"""
+Configuration and credentials management
+"""
+
+CONFIG = {
+    "openai_api_key": "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",
+    "default_advisor_email": "advisor@example.com"
+}
